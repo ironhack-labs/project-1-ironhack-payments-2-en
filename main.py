af53d7099b6ec9f6829e4b6976fafd0c76517df6
danisiaj <123707285+danisiaj@users.noreply.github.com>
2024-09-20 13:36:23 +0200

    updated Project

--- main.py
+++
@@ -1,3 +0,0 @@
-# ### Exploratory Data Analysis (EDA)
-
-# Before diving into cohort analysis, conduct an exploratory data analysis to gain a comprehensive understanding of the dataset. Explore key statistics, distributions, and visualizations to identify patterns and outliers. EDA will help you make informed decisions on data preprocessing and analysis strategies.
